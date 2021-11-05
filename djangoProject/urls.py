a1ea0b42cd8da083b9ce57f69d937dc8bf67bdb6
Maarten <mr.roos35@gmail.com>
2021-11-05 12:29:29 +0100

    Connect two dummy models to SQLite database

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# import django.urls
 from django.contrib import admin
 from django.urls import include, path
 
-# admin.autodiscover()
 
 urlpatterns = [
     path(r'productAdmin/', include('productAdmin.urls')),
